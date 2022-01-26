a8cad45d954bc8833100d3dbd9fe2665d5a33dd7
chalkchaser <kebintieu@gmail.com>
2022-01-26 17:38:34 +0100

    creadted test data for display

--- wgelo_frontend/src/App.js
+++ wgelo_frontend/src/App.js
@@ -1,7 +1,38 @@
 
+
 function App() {
+  const data = {
+    persons: [
+      {
+        name: 'Captain Falakofalako',
+        wins: 62,
+        losses: 49,
+        elo: 1240
+      },
+      {
+        name: 'Carlos Magnussen',
+        wins: 322,
+        losses: 54,
+        elo: 2844
+      },
+      {
+        name: 'Dan Hibiki',
+        wins: 12,
+        losses: 86,
+        elo: 840
+      },
+      {
+        name: 'Hasko Curly',
+        wins: 65,
+        losses: 87,
+        elo: 1336
+      }
+    ]
+  }
+
+
   return (
-    <div>start of wgelo</div>
+    <div>{data.persons[1].name}</div>
   );
 }
 
