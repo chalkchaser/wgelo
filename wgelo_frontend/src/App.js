d4cf98153fafd2805f48281338a9e30931526132
chalkchaser <kebintieu@gmail.com>
2022-02-10 02:22:52 +0100

    added player object creation to form submit

--- wgelo_frontend/src/App.js
+++ wgelo_frontend/src/App.js
@@ -1,8 +1,7 @@
- import './App.css'
- import { useEffect, useState } from 'react'
+import './App.css'
+import { useEffect, useState } from 'react'
 
-
- const data = {
+const data = {
   persons: [
     {
       name: 'Captain Falakofalako',
@@ -50,160 +49,154 @@
 }
 
 
- const Button = (props) => {
+const Button = (props) => {
   return (
     <button onClick={props.onClick}>
-      <div  className='buttonText'>{props.text}</div> 
+      <div className='buttonText'>{props.text}</div>
     </button>
   )
 }
 
 
-
-
-const PersonTable = ({persons, sortBy, setSortBy}) =>{
+const PersonTable = ({ persons, sortBy, setSortBy }) => {
 
   persons.sort((a, b) => b.elo - a.elo)//sort by higher elo to calculate ranking
-  persons.map((person, index) => person.rank = index+1)// add ranking
-  persons.map((person) => person.percentage = person.wins/(person.wins + person.losses))// add ranking
+  persons.map((person, index) => person.rank = index + 1)// add ranking
+  persons.map((person) => person.percentage = person.wins / (person.wins + person.losses))// add ranking
 
-
-  if(sortBy === 'elo'){
+  if (sortBy === 'elo') {
     //do nothing, since sort has already happened
   }
 
-  if(sortBy === 'name'){
+  if (sortBy === 'name') {
     persons.sort((a, b) => a.name.localeCompare(b.name))
     console.log("sort by name")
   }
 
-  if(sortBy === 'percentage'){
+  if (sortBy === 'percentage') {
     persons.sort((a, b) => b.percentage - a.percentage)
     console.log("sort by name")
   }
 
-  return(
+  return (
 
     <table id='personTable'>
-    <thead>
-      <tr>
-        <td onClick={()=> setSortBy('elo')}>#</td>
-        <td onClick={()=> setSortBy('name')}>name</td>
-        <td onClick={()=> setSortBy('percentage')}>W/L(%)</td>
-        <td onClick={()=> setSortBy('elo')}>elo</td>
+      <thead>
+        <tr>
+          <td onClick={() => setSortBy('elo')}>#</td>
+          <td onClick={() => setSortBy('name')}>name</td>
+          <td onClick={() => setSortBy('percentage')}>W/L(%)</td>
+          <td onClick={() => setSortBy('elo')}>elo</td>
         </tr>
-</thead>
-<tbody>
-      {persons.map(person =><tr key={person.name}>
-        <td>{person.rank}</td>
-        <td>{person.name}</td>
-        <td>{person.wins} - {person.losses}({(person.wins/(person.wins + person.losses)).toFixed(2)})</td>
-        <td>{person.elo}</td>
+      </thead>
+      <tbody>
+        {persons.map(person => <tr key={person.name}>
+          <td>{person.rank}</td>
+          <td>{person.name}</td>
+          <td>{person.wins} - {person.losses}({(person.wins / (person.wins + person.losses)).toFixed(2)})</td>
+          <td>{person.elo}</td>
         </tr>)}
-    
-</tbody>
-</table>
+
+      </tbody>
+    </table>
   )
 }
 
-const Navigation = ({setNavigate}) => {
+const Navigation = ({ setNavigate }) => {
 
-  return(
+  return (
     <div id='navigation'>
-      <Button class  text= "standings" onClick = {() => setNavigate("standings")}></Button>
-      <Button text= "record game" onClick = {() => setNavigate("record_game")}></Button>
-      <Button text= "history" onClick={() => setNavigate("history")}></Button>
+      <Button class text="standings" onClick={() => setNavigate("standings")}></Button>
+      <Button text="record game" onClick={() => setNavigate("record_game")}></Button>
+      <Button text="history" onClick={() => setNavigate("history")}></Button>
     </div>
   )
 }
 
-const Display = ({navigate,players, sortBy, setSortBy}) =>{
-  if(navigate === "standings"){
-    return(
-      <PersonTable persons = {players} sortBy={sortBy} setSortBy={setSortBy}/>
-    )
- 
-  }
-  else{
-    return(<div></div>)
-  }
-  
 
+const Display = ({ navigate, players, sortBy, setSortBy }) => {
+  if (navigate === "standings") {
+    return (
+      <PersonTable persons={players} sortBy={sortBy} setSortBy={setSortBy} />
+    )
+
+  }
+  else {
+    return (<div></div>)
+  }
 }
 
 
+const PlayerAddButton = ({ setPlayerform }) => {
 
-const PlayerAddButton = ({setPlayerform}) => {
-  const testPlayer ={
-    name: 'Test Player',
-    wins: 123,
-    losses: 232,
-    elo: 1452
-  }
-  return(
-    <Button text = "+player" onClick = {(()=> setPlayerform(true))} ></Button>
-    )
+  return (
+    <Button text="+player" onClick={(() => setPlayerform(true))} ></Button>
+  )
 }
 
-const onSubmit = (e) => {
+const addCreatedPlayer = (playerFormContent) => (e) => {
   e.preventDefault()
-  console.log("submit has been pressed")
+  const playerObject = {
+    name: playerFormContent,
+    wins: 0,
+    losses: 0,
+    elo: 1200
+  }
+  console.log(playerObject)
 }
 
-const PlayerAddWindow = ({playerForm}) =>{
+const PlayerAddWindow = ({ playerForm }) => {
 
-  const[playerFormContent, setPlayerFromContent] = useState('asdads')
+  const [playerFormContent, setPlayerFromContent] = useState('asdads')
 
   const handlePlayerFormOnChange = (event) => {
     console.log(event.target.value)
     setPlayerFromContent(event.target.value)
   }
 
-  if(playerForm)
-{
-  return(
-    <form onSubmit={onSubmit}>
-    <label>
-    Name:
-    <input type="text" name="name" value={playerFormContent} onChange={handlePlayerFormOnChange} />
-  </label>
-  <input type="submit" value= "submit" />
-</form>
-  )
-}else{
-  return(<></>)
-}
+  if (playerForm) {
+    return (
+      <form onSubmit={addCreatedPlayer(playerFormContent)}>
+        <label>
+          Name:
+          <input type="text" name="name" value={playerFormContent} onChange={handlePlayerFormOnChange} />
+        </label>
+        <input type="submit" value="submit" />
+      </form>
+    )
+  } else {
+    return (<></>)
+  }
 }
 
 function App() {
   const [players, setPlayers] = useState([])
-  const[navigate, setNavigate] = useState('standings')
-  const [sortBy, setSortBy] = useState('elo') 
-  const[playerForm,setPlayerform] = useState(false)
+  const [navigate, setNavigate] = useState('standings')
+  const [sortBy, setSortBy] = useState('elo')
+  const [playerForm, setPlayerform] = useState(false)
 
-  
 
-  const addPlayer = (player) =>{
+
+  const addPlayer = (player) => {
     setPlayers(players.concat(player))
   }
 
- 
-  useEffect(() => { setPlayers(data.persons)},[])
 
+  useEffect(() => { setPlayers(data.persons) }, [])
 
   return (
-    <div id = "all">
-    <div id = "main">
-    <Navigation setNavigate={setNavigate}></Navigation>
-   
-    <Display navigate = {navigate} players = {players} sortBy={sortBy} setSortBy={setSortBy}></Display>
-    </div>
+    <div id="all">
+      <div id="main">
+        <Navigation setNavigate={setNavigate}></Navigation>
+
+        <Display navigate={navigate} players={players} sortBy={sortBy} setSortBy={setSortBy}></Display>
+      </div>
 
       <div id='sideBar'>
-      <PlayerAddButton setPlayerform={setPlayerform}/>
-      <PlayerAddWindow playerForm = {playerForm}/>
+        <PlayerAddButton setPlayerform={setPlayerform} />
+        <PlayerAddWindow playerForm={playerForm} />
       </div>
-      </div>
+    </div>
 
   );
 }
