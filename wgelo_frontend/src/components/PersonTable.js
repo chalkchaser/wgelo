7b470b2508946d2e34ee302652eb909cee938192
chalkchaser <kebintieu@gmail.com>
2022-03-23 00:07:03 +0100

    added player page numbering splitter to reduce scrolling

--- wgelo_frontend/src/components/PersonTable.js
+++ wgelo_frontend/src/components/PersonTable.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useState } from 'react';
 
 const PersonTable = ({ persons, sortBy, setSortBy }) => {
+    const [selectedPerson, setSelectedPerson] = useState()
+
+
+
 
     persons.sort((a, b) => b.elo.at(-1) - a.elo.at(-1))//sort by higher elo to calculate ranking
     persons.map((person, index) => person.rank = index + 1)// add ranking
@@ -19,9 +24,15 @@ const PersonTable = ({ persons, sortBy, setSortBy }) => {
       persons.sort((a, b) => b.percentage - a.percentage)
       console.log("sort by name")
     }
+
+    let numbers = [];
+    for (let i = 1;  i-1 < persons.length/16; i++) {
+      numbers.push(<span>{i}</span>);
+    }
   
+    if(!selectedPerson){
     return (
-  
+      <div id ="tableContainer">
       <table id='personTable'>
         <thead>
           <tr>
@@ -32,7 +43,7 @@ const PersonTable = ({ persons, sortBy, setSortBy }) => {
           </tr>
         </thead>
         <tbody>
-          {persons.map(person => <tr key={person.name}>
+          {persons.slice(0,16).map(person => <tr key={person.name}>
             <td>{person.rank}</td>
             <td>{person.name}</td>
             <td>{person.wins} - {person.losses}({(person.wins / (person.wins + person.losses)).toFixed(2)})</td>
@@ -41,7 +52,12 @@ const PersonTable = ({ persons, sortBy, setSortBy }) => {
   
         </tbody>
       </table>
+      <div>{numbers}</div>
+      </div>
     )
+  }else{
+    return(<div class="playerDetail">{selectedPerson.name}</div>)
   }
+}
   
   export default PersonTable
