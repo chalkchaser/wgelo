db015e26011d378665e9c5c70affbab50329166f
chalkchaser <kebintieu@gmail.com>
2022-04-19 23:23:34 +0200

    added localstorage save of credentials

--- wgelo_frontend/src/index.js
+++ wgelo_frontend/src/index.js
@@ -11,6 +11,7 @@ ReactDOM.render(
     audience="https://chalkchaser.eu.auth0.com/api/v2/"
     scope="read:current_user update:current_user_metadata"
     useRefreshTokens= "true"
+    cacheLocation= "localstorage"
   >
     <App />
   </Auth0Provider>,
